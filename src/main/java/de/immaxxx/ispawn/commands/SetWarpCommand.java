713b9f1d5d3e35f0f23959fabfd053eadc67d927
ImMaxxx <immaxxx2304@gmail.com>
2023-06-14 00:27:04 +0200

    Update V3.8

--- src/main/java/de/immaxxx/ispawn/commands/SetWarpCommand.java
+++ src/main/java/de/immaxxx/ispawn/commands/SetWarpCommand.java
@@ -15,6 +15,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Locale;
 import java.util.stream.Collectors;
 
 public class SetWarpCommand implements TabExecutor {
